06eef567f5520cb61c903648b307f92e1c46eb57
Tim <tim@tim-specht.de>
2013-11-25 20:19:33 +0100

    testing auto-build

--- Hello WorldTests/Hello_WorldTests.m
+++ Hello WorldTests/Hello_WorldTests.m
@@ -26,9 +26,9 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testFail;
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    //XCTFail(@"Test failing builds ...");
 }
 
 @end
